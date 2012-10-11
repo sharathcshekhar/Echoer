7d21983c96b04b6196430ba2e4e002f06934c641
Sharath <sharathcshekhar@gmail.com>
2012-10-11 15:18:47 -0400

    My changes

--- TestServer.java
+++ TestServer.java
@@ -3,17 +3,15 @@ import java.net.*;
 import java.util.ArrayList;
 
 public class TestServer{
-	private static Object monitor12 = new Object();
+//	private static Object monitor12 = new Object();
 	
 	private static int myPortNumber;
-	private static final int MAX_CONNECTIONS = 7;
-	private static int otherPortNumber;
-	private ArrayList<ConnectionStatus> OutGoingConnections;
+//	private static int otherPortNumber;
+	private static ArrayList<ConnectionStatus> OutGoingConnections;
 	public static void main(String[] args){
 		String myPortNoStr = args[0];
-		String otherPortNoStr = args[1];
 		myPortNumber = Integer.parseInt(myPortNoStr);
-		otherPortNumber = Integer.parseInt(otherPortNoStr);
+		
 		Thread s = new Thread(){
 			public void run() {
 				try {
@@ -43,86 +41,93 @@ public class TestServer{
 public static void cli() throws InterruptedException, IOException{
 		
 	System.out.println("I am CLI thread");
-	/*class clientThread implements Runnable {
-			private Socket clientSocket;
-			public clientThread() {
-				//this.clientSocket = clientSocket;
-			}
-
-			public void run() {
-				try {
-				client();
-				} catch (IOException | InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-	}; */
-
 	BufferedReader cmdFromUser = new BufferedReader(new InputStreamReader(System.in));
 	while(true){
 		System.out.println("Enter the command:>");
-		String cmd = cmdFromUser.readLine();
-		String [] cmd_args = cmd.split(" ");
-		//synchronized(monitor12) {
-			switch(cmd_args[0]){
-				case "C":
-					//TODO store entered params in the ConnecetionStatus
-					//Conenct()
-					System.out.println("Connection IP requested is " + cmd_args[1]);
-					System.out.println("Connection to Port requested is " + cmd_args[2]);
-		//			Thread s2 = new Thread(new clientThread());
-		//			s2.start();
-					break;
-				case "S":
-					//TODO send message to the connection ID by iterating
-					System.out.println("Connection ID requested is " + cmd_args[1]);
-					System.out.println("Message to be sent is " + cmd_args[2]);
-		//			monitor12.notify();
-					break;
-				default:
-					System.out.println("Invalid command");
-					break;
+		String usrInput = cmdFromUser.readLine();
+		String [] cmd_args = usrInput.split(" ");
+		cmdEnum cmd = cmdEnum.valueOf(cmd_args[0].toUpperCase());
+
+			switch (cmd) {
+			case CONNECT:
+				
+				// TODO store entered params in the ConnecetionStatus
+				System.out.println("Connection IP requested is " + cmd_args[1]);
+				System.out.println("Connection to Port requested is "
+						+ cmd_args[2]);
+				Socket clientSocket = new Socket(cmd_args[1], Integer.parseInt(cmd_args[2]));
+				break;
+			case SEND:
+				// TODO send message to the connection ID by iterating
+				System.out.println("Connection ID requested is " + cmd_args[1]);
+				System.out.println("Message to be sent is " + cmd_args[2]);
+
+				String msgToSend = cmd_args[2].substring(cmd_args[2].indexOf(" "));
+
+				Socket sessionSocket = null;
+				sessionSocket = getClientSocketByConnectionID(cmd_args[1]);
+				BufferedReader fromServer = new BufferedReader(new InputStreamReader(sessionSocket.getInputStream()));
+				DataOutputStream toServer = new DataOutputStream(sessionSocket.getOutputStream());
+				System.out.println("cmd to send data received");
+				toServer.writeBytes(msgToSend + '\n');
+				String serverReply = fromServer.readLine();
+				System.out.println("Server replied with " + serverReply);
+				break;
+			case SENDTO:
+				break;
+			case INFO:
+				break;
+			case DISCONNECT:
+				break;
+			default:
+				System.out.println("Invalid command");
+				break;
 			}
-		//}
+		}
 	}
+
+	private static Socket getClientSocketByConnectionID(String string) {
+	// TODO Auto-generated method stub
+	return null;
 }
-private static void server() throws InterruptedException {
-			Socket clientSocket = new Socket();
-			System.out.println("My name is Server");
-			ServerSocket EchoerTCP = null;
-	   		try {
-       			EchoerTCP = new ServerSocket(myPortNumber);
-       		} catch (IOException e) {
-				System.out.println(e);
-        	}
-	   		class serverThread implements Runnable {
-  				private Socket clientSocket;
-  				public serverThread(Socket clientSocket) {
-     				this.clientSocket = clientSocket;
-  				}
 
-  				public void run() {
-  					try {
-						serverResponse(clientSocket);
-					} catch (IOException | InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-					}
-  				}
-			};
-
-			while(true){
-				try {
-					clientSocket = EchoerTCP.accept();
-				} catch (IOException e) {
-					System.out.println("Accept failed at " + myPortNumber);
+	private static void server() throws InterruptedException {
+		class serverThread implements Runnable {
+				private Socket clientSocket;
+				
+				public serverThread(Socket clientSocket) {
+					this.clientSocket = clientSocket;
 				}
-			
-				//TODO store the connection information
+
+				public void run() {
+					try {
+					serverResponse(clientSocket);
+					} catch (IOException | InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+		};
+		
+		Socket clientSocket = new Socket();
+		System.out.println("My name is Server");
+		ServerSocket EchoerTCP = null;
+		try {
+			EchoerTCP = new ServerSocket(myPortNumber);
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+
+		while (true) {
+			try {
+				clientSocket = EchoerTCP.accept();
+			} catch (IOException e) {
+				System.out.println("Accept failed at " + myPortNumber);
+			}
+			// TODO store the connection information
 			Thread s3 = new Thread(new serverThread(clientSocket));
 			s3.start();
-			}
+		}
 	}
 
 	public static void serverResponse(Socket clientSocket) throws IOException, InterruptedException
@@ -130,31 +135,19 @@ private static void server() throws InterruptedException {
 		System.out.println("Socket connection accepted, reading from the socket");
 		BufferedReader fromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		DataOutputStream toClient = new DataOutputStream(clientSocket.getOutputStream());
-		while(true) { //TODO while(connection is active)
+		while(true) { 
 			String clientMsg = fromClient.readLine();
+			if(clientMsg == null) {
+				System.out.println("Client has disconnected");
+				break;
+			}
 			System.out.println("Client says " + clientMsg + "Server Echoes the same");
 			toClient.writeBytes(clientMsg + '\n');
-			
 		}
+		fromClient.close();
+		toClient.close();
 	}
-/*	
-	public static void client() throws IOException, InterruptedException
-	{
-		System.out.println("making connection to localhost:" + otherPortNumber);
-		Socket serverSocket = new Socket("localhost", otherPortNumber);
-		BufferedReader fromServer = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
-		DataOutputStream toServer = new DataOutputStream(serverSocket.getOutputStream());
-		System.out.println("Connected to server, waiting to send data");
-		String clientMsg = "hello";
-		while(true){
-			synchronized(monitor12) {
-				monitor12.wait();
-			}
-			System.out.println("cmd to send data received. Saying hello to server");
-			toServer.writeBytes(clientMsg + '\n');
-			String serverReply = fromServer.readLine();
-			System.out.println("Server replied with " + serverReply);
-		}
+	public enum cmdEnum {
+		CONNECT, SEND, SENDTO, SHOW, INFO, DISCONNECT, INVALID
 	}
-*/
 }
