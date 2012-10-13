6bbb943ffdc3c8a00ac663ce55074a18d7822cd7
Sanket <esanketkulkarni@gmail.com>
2012-10-13 05:14:10 -0400

    latest

--- ConnectionStatus.java
+++ ConnectionStatus.java
@@ -1,53 +1,57 @@
 import java.net.Socket;
 
 public class ConnectionStatus {
-private int connectionID;
-private String ip, hostname;
-private int localPort,remotePort;
-private boolean isActive;
-private Socket clientSocket;
+	private int connectionID;
+	private String ip, hostname;
+	private int localPort, remotePort;
+	private Socket clientSocket;
 
-public int getConnectionID() {
-return connectionID;
-}
-public void setConnectionID(int connectionID) {
-this.connectionID = connectionID;
-}
-public String getIp() {
-return ip;
-}
-public void setIp(String ip) {
-this.ip = ip;
-}
-public String getHostname() {
-return hostname;
-}
-public void setHostname(String hostname) {
-this.hostname = hostname;
-}
-public int getLocalprt() {
-return localPort;
-}
-public void setLocalprt(int localPort) {
-this.localPort = localPort;
-}
-public int getRemoteport() {
-return remotePort;
-}
-public void setRemoteport(int remotePort) {
-this.remotePort = remotePort;
-}
-public Socket getClientSocket() {
-	return clientSocket;
-}
-public void setClientSocket(Socket clientSocket) {
-	this.clientSocket = clientSocket;
-}
-public boolean isActive() {
-return isActive;
-}
-public void setActive(boolean isActive) {
-this.isActive = isActive;
-}
+	public int getConnectionID() {
+		return connectionID;
+	}
+
+	public void setConnectionID(int connectionID) {
+		this.connectionID = connectionID;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public String getHostname() {
+		return hostname;
+	}
+
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+
+	public int getLocalprt() {
+		return localPort;
+	}
+
+	public void setLocalprt(int localPort) {
+		this.localPort = localPort;
+	}
+
+	public int getRemoteport() {
+		return remotePort;
+	}
+
+	public void setRemoteport(int remotePort) {
+		this.remotePort = remotePort;
+	}
+
+	public Socket getClientSocket() {
+		return clientSocket;
+	}
+
+	public void setClientSocket(Socket clientSocket) {
+		this.clientSocket = clientSocket;
+	}
 
 }
