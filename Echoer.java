2baf251fbf6ede276e7b4637dac8034ab3d5931b
Sharath <sharathcshekhar@gmail.com>
2012-10-12 23:31:35 -0400

    Added method to get Local IP, more sanity on Echoer.java

--- Echoer.java
+++ Echoer.java
@@ -11,89 +11,87 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Iterator;
+//import java.util.logging.Logger;
 
 public class Echoer {
 
 	private static ArrayList<ConnectionStatus> OutGoingConnections = new ArrayList<ConnectionStatus>();
-
+	
 	public static void main(String[] args) {
 		int counterConnections = 0;
-		int myPortNumber = 0;
-		int myUDPPortNumber = 0;
+		int tcpServerPort = 0;
+		int udpServerPort = 0;
 		if(args.length != 2) {
-			System.out.println("Incorrect number of arguments. Eg \"Echoer 4242 4343\"\n");
-			System.exit(0);
+			System.out.println("Incorrect number of arguments. Eg: \"java Echoer 4242 4343\"\n");
+			System.exit(1);
 		}
-		
 		try {
-			myPortNumber = Integer.parseInt(args[0]);
-			myUDPPortNumber = Integer.parseInt(args[1]);
+			tcpServerPort = Integer.parseInt(args[0]);
+			udpServerPort = Integer.parseInt(args[1]);
 		} catch (NumberFormatException e) {
 			System.out.println("Illigal input, TCP and UDP port numbers have to be numbers\n");
-			System.exit(0);
+			System.exit(1);
 		}
 		
-		if(myPortNumber > 65535 || myPortNumber < 1025) {
+		if(tcpServerPort > 65535 || tcpServerPort < 1025) {
 			System.out.println("TCP Port number out of Range\n");
-			System.exit(0);
+			System.exit(1);
 		}
 		
-		if(myUDPPortNumber > 65535 || myUDPPortNumber < 1025) {
+		if(udpServerPort > 65535 || udpServerPort < 1025) {
 			System.out.println("UDP port number out of Range\n");
-			System.exit(0);
+			System.exit(1);
 		}
 		
-		if(myPortNumber == myUDPPortNumber){
+		if(tcpServerPort == udpServerPort){
 			System.out.println("Enter different port numbers for TCP and UDP\n");
-			System.exit(0);
+			System.exit(1);
 		}
-		
-		
-	
+
 		InetAddress addr = null;
 		try {
 			addr = InetAddress.getLocalHost();
 		} catch (UnknownHostException e) {
 			System.out.println("Unable to get the IP of the machine, exiting\n");
-			System.exit(0);
+			System.exit(1);
 		}
 
-		class server implements Runnable {
+		class TCPServer implements Runnable {
 			private int portNumber;
 
-			public server(int portNumber) {
+			public TCPServer(int portNumber) {
 				this.portNumber = portNumber;
 			}
 
 			public void run() {
-				serverThread(portNumber);
+				TCPServerThread(portNumber);
 			}
 		}
 		;
 
-		class UDPserver implements Runnable {
+		class UDPServer implements Runnable {
 			private int portNumber;
 
-			public UDPserver(int portNumber) {
+			public UDPServer(int portNumber) {
 				this.portNumber = portNumber;
 			}
 
 			public void run() {
-					UDPserverThread(portNumber);
+					UDPServerThread(portNumber);
 			}
 		}
 		;
 
-		Thread s1 = new Thread(new server(myPortNumber));
-		s1.start();
-		Thread udp_t = new Thread(new UDPserver(myUDPPortNumber));
+		Thread tcp_t = new Thread(new TCPServer(tcpServerPort));
+		tcp_t.start();
+		Thread udp_t = new Thread(new UDPServer(udpServerPort));
 		udp_t.start();
 
 		BufferedReader cmdFromUser = new BufferedReader(new InputStreamReader(
 				System.in));
 		System.out.println("I am CLI:");
 		while (true) {
-			System.out.print("Enter a command: >");
+			System.out.print("Echoer>");
 			String usrInput = null;
 			try {
 				usrInput = cmdFromUser.readLine();
@@ -113,6 +111,11 @@ public class Echoer {
 			}
 			switch (cmd) {
 			case CONNECT:
+				if(cmd_args.length != 3)
+				{
+					System.out.println("Wrong arguments to connect");
+					break;
+				}
 				if (!ValidateIP.validate(cmd_args[1])) {
 					System.out.println("Invalid IPv4 format\n");
 				}
@@ -146,13 +149,18 @@ public class Echoer {
 				connectionStatus.setIp(clientSocket.getInetAddress()
 						.getHostAddress());
 				connectionStatus.setRemoteport(clientSocket.getPort());
-				connectionStatus.setLocalprt(myPortNumber);
+				connectionStatus.setLocalprt(tcpServerPort);
 				connectionStatus.setClientSocket(clientSocket);
 				// add connection status to list
 				OutGoingConnections.add(connectionStatus);
 				}
 				break;
 			case SEND:
+				if(cmd_args.length < 3)
+				{
+					System.out.println("Too few arguments to send");
+					break;
+				}
 				System.out.println("Connection ID requested is " + cmd_args[1]);
 
 				// Shamefully ugly!
@@ -186,6 +194,11 @@ public class Echoer {
 				System.out.println("Server replied with " + serverReply);
 				break;
 			case SENDTO:
+				if(cmd_args.length < 4)
+				{
+					System.out.println("Too few arguments to sendto");
+					break;
+				}
 				if (!ValidateIP.validate(cmd_args[1])) {
 					System.out.println("Invalid IPv4 format\n");
 					continue;
@@ -220,6 +233,11 @@ public class Echoer {
 				}}
 				break;
 			case SHOW:
+				if(cmd_args.length > 1)
+				{
+					System.out.println("Too many arguments to show");
+					break;
+				}
 				Iterator<ConnectionStatus> itr = OutGoingConnections.iterator();
 				while (itr.hasNext()) {
 					ConnectionStatus connectionItr = itr.next();
@@ -232,12 +250,22 @@ public class Echoer {
 				}
 				break;
 			case INFO:
+				if(cmd_args.length > 1)
+				{
+					System.out.println("Too many arguments to info");
+					break;
+				}
 				System.out.println("\nIP Address=" + addr.getHostAddress()
 						+ "\tHost Name=" + addr.getHostName() + "\tTCP Port="
-						+ myPortNumber + "\tUDP Port=" + myUDPPortNumber);
+						+ tcpServerPort + "\tUDP Port=" + udpServerPort);
 
 				break;
 			case DISCONNECT:
+				if(cmd_args.length != 2)
+				{
+					System.out.println("Wrong arguments to Disconnect");
+					break;
+				}
 				try {
 				int connectionID = Integer.parseInt(cmd_args[1]);
 				getClientSocketByConnectionID(connectionID)
@@ -261,6 +289,9 @@ public class Echoer {
 				}
 				recentCount();
 				break;
+			case BYE:
+				System.exit(0);
+				break;
 			default:
 				System.out.println("Invalid command");
 				break;
@@ -274,7 +305,7 @@ public class Echoer {
 		}
 	}
 
-	private static void serverThread(int myPortNumber) {
+	private static void TCPServerThread(int tcpServerPort) {
 		class serverResponseThread implements Runnable {
 			private Socket clientSocket;
 
@@ -283,7 +314,6 @@ public class Echoer {
 			}
 
 			public void run() {
-
 				serverResponse(clientSocket);
 			}
 		}
@@ -293,17 +323,19 @@ public class Echoer {
 		System.out.println("My name is Server");
 		ServerSocket EchoerTCP = null;
 		try {
-			EchoerTCP = new ServerSocket(myPortNumber);
+			EchoerTCP = new ServerSocket(tcpServerPort);
 		} catch (SocketException e) {
 			System.out.println("TCP Socket already in use.");
+			System.exit(1);
 		} catch (IOException e) {
-			System.out.println("Accept failed at " + myPortNumber);
+			System.out.println("Accept failed at " + tcpServerPort);
+			System.exit(1);
 		} 
 		while (true) {
 			try {
 				clientSocket = EchoerTCP.accept();
 			} catch (IOException e) {
-				System.out.println("Accept failed at " + myPortNumber);
+				System.out.println("Accept failed at " + tcpServerPort);
 				continue;
 			}
 			// TODO store the connection information
@@ -354,7 +386,7 @@ public class Echoer {
 	}
 
 	public enum cmdEnum {
-		CONNECT, SEND, SENDTO, SHOW, INFO, DISCONNECT, INVALID
+		CONNECT, SEND, SENDTO, SHOW, INFO, DISCONNECT, BYE, INVALID
 	}
 
 	public static Socket getClientSocketByConnectionID(int connectionId) {
@@ -368,7 +400,7 @@ public class Echoer {
 		return null;
 	}
 
-	private static void UDPserverThread(int UDPport) {
+	private static void UDPServerThread(int UDPport) {
 		System.out.println("My name is UDP Server");
 		DatagramSocket EchoerUDPSocket = null;
 		try {
--- ValidateIP.java
+++ ValidateIP.java
@@ -1,4 +1,10 @@
 
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
  
@@ -21,4 +27,59 @@ public class ValidateIP{
     matcher = pattern.matcher(ip);
     return matcher.matches();	    	    
     }
+   
+ public static String getLocalIPAddress() 
+ {
+	 InetAddress IPAddress = null;
+	 /* 
+	  * Method suggested by TA
+	  * Looks like both these peices of code is working only on linux machines and not
+	  * on windows. May be you have to do another check for OS if you care.
+	  * Strictly it need not be UDP also. You can do a TCP connect() to google.com 
+	  * as I told. He TA says UDP, make him happy.
+	  */
+	try {
+		IPAddress = InetAddress.getByName("8.8.8.8");
+	} catch (UnknownHostException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}
+ 	DatagramSocket clientUDPSocket = null;
+	try {
+		clientUDPSocket = new DatagramSocket();
+	} catch (SocketException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}
+		
+		clientUDPSocket.connect(IPAddress, 53); //53 is the well-known port number used by DNS
+		InetAddress IP = clientUDPSocket.getLocalAddress();
+ 	
+		  System.out.println("IP udp:" + IP.getHostAddress());
+    /* End of method suggested by TA */
+	
+	// NOTE: Keep both the code.. and comment out the code you don't like.
+	// We can test the code and keep the one that works
+		
+	/* A better Method in all ways */
+	
+	Enumeration<NetworkInterface> nis;
+	try {
+		nis = NetworkInterface.getNetworkInterfaces();
+	} catch (SocketException e) {
+		return null;
+	}
+    while(nis.hasMoreElements()){
+    	NetworkInterface ni = nis.nextElement();
+    	Enumeration<InetAddress> inetAddresses =  ni.getInetAddresses();
+    	while(inetAddresses.hasMoreElements()) {
+    		InetAddress ia = inetAddresses.nextElement();
+    		if(!ia.isLinkLocalAddress() && !ia.isLoopbackAddress()) {
+    			return ia.getHostAddress();
+    		}
+    	}
+    }
+    return null;
+}
+
 }
